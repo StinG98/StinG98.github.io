afb38882305f7964213d2db64220bbaf68f4b8f4
Gauvain SOU <sou@d17-l427-pc-02.ipa.iisg.agh.edu.pl>
2019-03-18 09:11:26 +0100

    My 11th  commit

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ function greet() {
 function init() {
 	let images = document.getElementById("container").getElementsByTagName("img");
 	document.getElementById("first").src="car2.png";
-	images[1].src="flower2.png";
-	images[3].src="flower2.png";
+	document.getElementById("container").getElementsByTagName("img")[1].src = "flower2.png";
+document.getElementById("container").getElementsByTagName("img")[3].src = "flower2.png";
+
 
 
 
