6154357330bdbb43a2ae1abb1e8369926f12c7b2
Gauvain SOU <sou@d17-l427-pc-02.ipa.iisg.agh.edu.pl>
2019-03-18 09:06:53 +0100

    My 9th  commit

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function greet() {
 	window.alert("Hello everyone!");
 }
-let images = document.getElementById("container").getElementsByTagName("img");
+
 function init() {
+	let images = document.getElementById("container").getElementsByTagName("img");
 	document.getElementById("first").src="car2.png";
 	image[1].src="flower2.png";
-
 	image[3].src="flower2.png";
 
 	}
